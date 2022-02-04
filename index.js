ccd0d85dc463a3082087b1aaf5361fe34ae2f1e7
Jackie Tung <jackietung3@gmail.com>
2022-02-04 14:47:31 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let i = 0;
+    const arrOfMessages = []
+    while (i < names.length){
+     arrOfMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+     i++;
+   }
+  return arrOfMessages
+}
+
+function countDown(number) {
+    let i = number; 
+    while (i>=0) {
+        console.log (i--)
+    }
+}
