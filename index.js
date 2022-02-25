c3a207c8dcb7a20b7e51314655d36349ad18c08a
Jackie Tung <jackietung3@gmail.com>
2022-02-25 12:28:43 -0500

    commit tree 7f29bb82685a89873cde078bf986356b614dd053

--- index.js
+++ index.js
@@ -13,4 +13,18 @@ function countDown(number) {
     while (i>=0) {
         console.log (i--)
     }
+    return i--
 }
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     debugger;
+//   }
+
+//   return gifts;
+// }
+
+// wrapGifts(gifts);
